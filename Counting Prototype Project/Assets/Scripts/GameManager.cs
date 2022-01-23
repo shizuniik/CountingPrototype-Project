43f88944a8fca957af82da61fb0746ab3309363b
shizuniik <97367206+shizuniik@users.noreply.github.com>
2022-01-23 17:33:18 -0300

    timer + game over text
    added contdown timer and game over text

--- Counting Prototype Project/Assets/Scripts/GameManager.cs
+++ Counting Prototype Project/Assets/Scripts/GameManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI; 
 
 public class GameManager : MonoBehaviour
@@ -9,20 +8,35 @@ public class GameManager : MonoBehaviour
     [SerializeField] List<GameObject> listItems;
     public GameObject player;
     public Text countText;
+    public Text timerText;
+    public Text gameOverText; 
     public bool gameOver; 
     private float spawnRate = 5.0f;
-
+    private float timeLeft = 30;
     // Start is called before the first frame update
     void Start()
     {
         gameOver = false;
-        StartCoroutine(SpawnItems()); 
+        StartCoroutine(SpawnItems());
+        timerText.text = "Time: " + timeLeft;
     }
 
     // Update is called once per frame
     void Update()
     {
-        countText.text = "Count: " + player.GetComponent<PlayerController>().Count.ToString();   
+        if (timeLeft < 0)
+        {
+            gameOver = true;
+            gameOverText.gameObject.SetActive(true);
+        }
+
+        if (!gameOver)
+        {
+            timeLeft -= Time.deltaTime;
+            timerText.text = "Time: " + Mathf.RoundToInt(timeLeft);
+
+            countText.text = "Count: " + player.GetComponent<PlayerController>().Count.ToString();
+        }
     }
 
     IEnumerator SpawnItems()
@@ -33,4 +47,5 @@ public class GameManager : MonoBehaviour
             Instantiate(listItems[Random.Range(0, listItems.Count)]);
         }
     }
+
 }
